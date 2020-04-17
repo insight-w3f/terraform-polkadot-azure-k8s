abc554a59e41d37fa11188dd5e835b30e1b02cd4
Richard Mah <dick.mah@gmail.com>
2020-04-17 15:59:08 -0400

    fix default test

--- examples/defaults/main.tf
+++ examples/defaults/main.tf
@@ -11,9 +11,15 @@ resource "azurerm_resource_group" "this" {
   name     = "default-test"
 }
 
+module "network" {
+  source                    = "github.com/insight-w3f/terraform-polkadot-azure-network.git?ref=master"
+  azure_resource_group_name = azurerm_resource_group.this.name
+}
+
 module "defaults" {
   source                             = "../.."
   azure_resource_group_name          = azurerm_resource_group.this.name
   k8s_azure_service_principal_id     = var.k8s_sp_id_from_env
   k8s_azure_service_principal_secret = var.k8s_sp_sec_from_env
+  vpc_id                             = module.network.vpc_id
 }
